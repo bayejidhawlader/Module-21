7a9a9fe945b44a1deb43291f382f910a4507f231
bayejidhawlader <bayejidhawlader@gmail.com>
2022-08-07 16:29:24 +0600

    first commit

---
+++ length.js
@@ -0,0 +1,14 @@
+// var name = "Ahmed      "
+
+// console.log(name.length);
+
+
+const userName = 'bayejid';
+const userInput = 'ahmed123farhan';
+
+if (userName === userInput){
+    console.log('Valid User');
+}
+else{
+    console.log('Invalid User');
+}
---
+++ search.js
@@ -0,0 +1,5 @@
+const song = 'tumi bondhu kala pakhi ami jeno ki';
+
+const doesExist = song.includes('jenok   i');
+
+console.log(doesExist);
